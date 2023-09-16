ec76363047d6d5af6f10a61d06159c99ac63af09
Aleksey Kravchenko <alekseykravchenko120@gmail.com>
2023-09-16 23:12:50 +0300

    something new

---
+++ internet_programming_technology/labs/lab_2/Lab_2/src/main/java/by/kravchenko/lab_2/Model/Book.java
@@ -0,0 +1,13 @@
+package by.kravchenko.lab_2.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+}
