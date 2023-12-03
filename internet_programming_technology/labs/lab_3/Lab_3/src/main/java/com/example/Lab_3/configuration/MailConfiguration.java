f461068c5af1f024add7f5920fdce5cca72c54b2
Aleksey Kravchenko <alekseykravchenko120@gmail.com>
2023-12-03 14:20:15 +0300

    Lab number 3 for Java is completed!

---
+++ internet_programming_technology/labs/lab_3/Lab_3/src/main/java/com/example/Lab_3/configuration/MailConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.Lab_3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp-mail.outlook.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("TaskWave@outlook.com");
+        mailSender.setPassword("TWPassword");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
