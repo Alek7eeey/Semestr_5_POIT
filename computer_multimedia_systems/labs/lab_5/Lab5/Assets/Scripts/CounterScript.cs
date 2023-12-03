a27b52751617af887e0a2da0822357f53bef9759
Aleksey Kravchenko <alekseykravchenko120@gmail.com>
2023-12-03 14:22:30 +0300

    update unity lab

---
+++ computer_multimedia_systems/labs/lab_5/Lab5/Assets/Scripts/CounterScript.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+  public class CounterScript : MonoBehaviour
+  {
+    public TextMeshProUGUI counterText;
+    public GameObject player;
+    private static int _counter = 0;
+
+    void Start()
+    {
+      counterText.text = _counter.ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      if (other.gameObject != player) return;
+      
+      _counter++;
+      counterText.text = _counter.ToString();
+    }
+  }
+}
